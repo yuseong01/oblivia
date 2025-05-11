5a1ed244284f82ea8b04dba3db6f4cd4f5f346bd
yeji-gim <piggygim3@gmail.com>
2025-05-11 21:07:48 +0900

    feat: BreakalbeObstacle 기능 추가
    BreakalbeObstacle 기능 추가

--- Assets/Scripts/Obstacle/BreakableObstacle.cs
+++ Assets/Scripts/Obstacle/BreakableObstacle.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BreakableObstacle : BaseObstacle
+public class BreakableObstacle : BaseObstacle, ISpawnable
 {
-    [SerializeField] private GameObject dropItem;
-    [SerializeField] private int health = 1; // 체력 설정
+    [Header("보상 or 몬스터")]
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _rewardPrefab;
+    [SerializeField] private int _spawnCount = 2;
+    [SerializeField] private float _spawnRadius = 2f;
+
+    [SerializeField] private int _health = 1; // 체력 설정
 
     private void Awake()
     {
@@ -14,13 +19,28 @@ public class BreakableObstacle : BaseObstacle
 
     public override void OnInteract(GameObject interactor)
     {
-        health--;
-        if (health <= 0)
+        _health--;
+        if (_health <= 0)
         {
-            if (dropItem != null)
-                Instantiate(dropItem, transform.position, Quaternion.identity);
-
+            SpawnAround(transform.position);
             Destroy(gameObject);
         }
     }
+    private void SpawnAround(Vector3 center)
+    {
+        bool spawnType = Random.value < 0.5f;
+        GameObject type = spawnType ? _enemyPrefab : _rewardPrefab;
+        for (int i = 0; i < _spawnCount; i++)
+        {
+            Vector2 offset = Random.insideUnitCircle * _spawnRadius;
+            Vector3 pos = center + new Vector3(offset.x, 0f, offset.y);
+            Instantiate(type, pos, Quaternion.identity);
+        }
+    }
+
+    // ISpawnable 구현부
+    public GameObject GetSpawnPrefab() => Random.value < 0.5f ? _enemyPrefab : _rewardPrefab;
+    public int GetSpawnCount() => _spawnCount;
+    public float GetSpawnRadius() => _spawnRadius;
+
 }
