8778c0b97b0919c62ab0b9eed67d0137fe65b8c3
rudals4469 <80159080+rudals4469@users.noreply.github.com>
2025-05-12 17:09:43 +0900

    feat : 배경소리, 소리 조절 기능 구현

---
+++ Assets/Scripts/SaveRoad/UI_Settings.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Settings : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private bool isOpen => settingsPanel.activeSelf;
+
+    private void Start()
+    {
+        bgmSlider.value = PlayerPrefs.GetFloat("BGMVolume", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        SoundManager.Instance.SetBGMVolume(bgmSlider.value);
+        SoundManager.Instance.SetSFXVolume(sfxSlider.value);
+
+        bgmSlider.onValueChanged.AddListener(SetBGMVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+
+        settingsPanel.SetActive(false); // 시작할 때 비활성화
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isOpen)
+                CloseSettings();
+            else
+                OpenSettings();
+        }
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+        // Time.timeScale = 0f;
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        // Time.timeScale = 1f;
+    }
+
+    private void SetBGMVolume(float volume)
+    {
+        SoundManager.Instance.SetBGMVolume(volume);
+        PlayerPrefs.SetFloat("BGMVolume", volume);
+    }
+
+    private void SetSFXVolume(float volume)
+    {
+        SoundManager.Instance.SetSFXVolume(volume);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+}
