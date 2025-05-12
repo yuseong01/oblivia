abb8500c9f8dd36a016bbd8170200334b143e1bc
rudals4469 <80159080+rudals4469@users.noreply.github.com>
2025-05-12 09:29:05 +0900

    fix : door
    문 버그 수정

--- Assets/Scripts/Character/CharacterData.cs
+++ Assets/Scripts/Character/CharacterData.cs
@@ -5,10 +5,20 @@ public class CharacterData : ScriptableObject
 {
     public string playerName;
     public string characterName;
+    public string characterId; // 각 캐릭터 고유 ID (중요)
+
+    public float moveSpeed;
+
+    public float maxHealth;
+
     public float damage;
     public float attackRate;
     public float attackDelay;
     public float attackSpeed;
     public float attackRange;
-    public string characterId; // 각 캐릭터 고유 ID (중요)
+    public float attackCount;
+    public float attackAngle;
+    public float knockbackForce;
+    public float attackDuration;
+    public float projectileSize;
 }
